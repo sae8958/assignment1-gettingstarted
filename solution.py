cb1b6478dccaad1617abc4c10aaa84f3def8de0e
saeveritt <sae8958@nyu.edu>
2022-09-07 09:25:12 -0500

    Completed solution.py

--- solution.py
+++ solution.py
@@ -1,34 +1,15 @@
-### welcome_assignment_answers
-### Input - All nine questions given in the assignment.
-### Output - The right answer for the specific question.
+def welcome_assignment_answers(question: str):
+    questions = {
+        "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": "mtls",
+        "Are encoding and encryption the same? - Yes/No": "No",
+        "Is it possible to decrypt a message without a key? - Yes/No": "No",
+        "Is it possible to decode a message without a key? - Yes/No": "No",
+        "Is a hashed message supposed to be un-hashed? - Yes/No": "No",
+        "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code": "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b",
+        "Is MD5 a secured hashing algorithm? - Yes/No": "No",
+        "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": 4,
+        "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": 3,
 
-def welcome_assignment_answers(question):
-    #Students do not have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
-    else: 
-        ### you should understand why this else case should be included
-        ### what happens if there is a typo in one of the questions?
-        answer = ""
-    return(answer)
-# Complete all the questions.
-
-
-if __name__ == "__main__":
-    #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-
-#Questions:
-#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-#"Are encoding and encryption the same? - Yes/No":
-#"Is it possible to decrypt a message without a key? - Yes/No":
-#"Is it possible to decode a message without a key? - Yes/No":
-#"Is a hashed message supposed to be un-hashed? - Yes/No":
-#"What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
-#"Is MD5 a secured hashing algorithm? - Yes/No":
-#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+    }
+    
+    return questions.get(question, "Invalid question")
