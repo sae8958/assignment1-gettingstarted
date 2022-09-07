15b37d56a7589a508915ee53e8cd652a32dbac47
sae8958 <sae8958@nyu.edu>
2022-09-07 09:40:01 -0500

    update questions

--- solution.py
+++ solution.py
@@ -3,13 +3,13 @@ def welcome_assignment_answers(question: str):
         "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": "mtls",
         "Are encoding and encryption the same? - Yes/No": "No",
         "Is it possible to decrypt a message without a key? - Yes/No": "No",
-        "Is it possible to decode a message without a key? - Yes/No": "No",
+        "Is it possible to decode a message without a key? - Yes/No": "Yes",
         "Is a hashed message supposed to be un-hashed? - Yes/No": "No",
-        "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code": "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b",
+        "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code": "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b",
         "Is MD5 a secured hashing algorithm? - Yes/No": "No",
-        "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": 4,
+        "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": 5,
         "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": 3,
 
     }
     
-    return questions.get(question, "Invalid question")
+    return questions.get(question, "")
